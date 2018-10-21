7aa15726366f6914019d53f6b02c02da6cc6ba84
Sina Sharifzade <sharifzadesina@gmail.com>
2018-10-22 01:52:19 +0330

    Update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,7 +15,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         require(app_path('Rules/rules.php'));
-        
+
         Paginator::useBootstrapThree();
     }
 
