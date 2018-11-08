47fe49f9988dd85ad702964ee4e7909bcaf1967c
Sina Sharifzade <sharifzadesina@gmail.com>
2018-11-08 18:09:59 +0330

    Update

--- app/Support/helpers.php
+++ app/Support/helpers.php
@@ -1,8 +1,3 @@
 <?php
 
-if (!function_exists('cdn')) {
-    function cdn($path, $secure = null)
-    {
-        return asset(config('app.assets_url').(starts_with($path, '/') ? '' : '/').$path, $secure);
-    }
-}
+//
