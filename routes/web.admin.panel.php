fb82eb9b30a4c3e389e3c9bb5085cd32ad24ec75
Sina <sharifzadesina@gmail.com>
2019-07-20 23:17:03 +0430

    Use patch

--- routes/web.admin.panel.php
+++ routes/web.admin.panel.php
@@ -5,5 +5,5 @@ Route::patch('users/{user}/password', 'UserController@updatePassword')->name('us
 Route::resource('users', 'UserController')->except(['show', 'create', 'store', 'destroy']);
 
 Route::get('endusers/datatables', 'EnduserController@datatables')->name('endusers.datatables');
-Route::put('endusers/{customer}/password', 'EnduserController@updatePassword')->name('endusers.update.password');
+Route::patch('endusers/{customer}/password', 'EnduserController@updatePassword')->name('endusers.update.password');
 Route::resource('endusers', 'EnduserController');
