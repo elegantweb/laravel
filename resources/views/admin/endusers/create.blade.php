0e4affc7c03ab6df18ef00ffce454bdc5b553c7e
Sina <sharifzadesina@gmail.com>
2020-01-25 23:14:37 +0330

    Update create.blade.php

--- resources/views/admin/endusers/create.blade.php
+++ resources/views/admin/endusers/create.blade.php
@@ -10,7 +10,7 @@
     {{ csrf_field() }}
 
     <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-        <label for="name" class="control-label">Name</label>
+        <label for="name">Name</label>
         <input id="name" type="text" class="form-control"
                name="name" value="{{ old('name') }}" required>
         @if ($errors->has('name'))
@@ -21,7 +21,7 @@
     </div>
 
     <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-        <label for="email" class="control-label">Email</label>
+        <label for="email">Email</label>
         <input id="email" type="email" class="form-control"
                name="email" value="{{ old('email') }}" required>
         @if ($errors->has('email'))
@@ -32,7 +32,7 @@
     </div>
 
     <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-        <label for="password" class="control-label">Password</label>
+        <label for="password">Password</label>
         <input id="password" type="password" class="form-control" name="password" required>
         @if ($errors->has('password'))
             <span class="help-block">
@@ -42,7 +42,7 @@
     </div>
 
     <div class="form-group">
-        <label for="password-confirm" class="control-label">Confirm Password</label>
+        <label for="password-confirm">Confirm Password</label>
         <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
     </div>
 </form>
