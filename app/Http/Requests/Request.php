77ccfad8a51841d0edd6bbbdd2bc3ac2ad4bcf24
Sina Sharifzade <sharifzadesina@gmail.com>
2020-08-18 21:36:41 +0430

    Use elegantweb sanitizer

--- app/Http/Requests/Request.php
+++ app/Http/Requests/Request.php
@@ -2,10 +2,21 @@
 
 namespace App\Http\Requests;
 
+use Elegant\Sanitizer\Laravel\SanitizesInput;
 use Illuminate\Foundation\Http\FormRequest;
-use Waavi\Sanitizer\Laravel\SanitizesInput;
+use Illuminate\Contracts\Validation\Validator;
 
 class Request extends FormRequest
 {
     use SanitizesInput;
+
+    public function withValidator(Validator $validator)
+    {
+        $validator->after([$this, 'after']);
+    }
+
+    public function after(Validator $validator)
+    {
+        // pass
+    }
 }
