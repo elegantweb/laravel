55fc91a97a2dc0af9c69cc9bfc3fbbbc7e272332
Sina <sharifzadesina@gmail.com>
2020-09-12 15:43:55 +0430

    Update admin.panel.php

--- routes/admin.panel.php
+++ routes/admin.panel.php
@@ -1,18 +1,12 @@
 <?php
 
-Route::ddd('employees')->group(function () {
+use App\Admin\Clients\Controllers\ClientController;
+use App\Admin\Employees\Controllers\EmployeeController;
 
-    Route::get('employees/dataTables', 'EmployeeController@dataTables')->name('employees.dataTables');
-    Route::patch('employees/{employee}/password', 'EmployeeController@updatePassword')->name('employees.update.password');
-    Route::resource('employees', 'EmployeeController')->except(['show', 'create', 'store', 'destroy']);
+Route::get('employees/dataTables', [EmployeeController::class, 'dataTables'])->name('employees.dataTables');
+Route::patch('employees/{employee}/password', [EmployeeController::class, 'updatePassword'])->name('employees.update.password');
+Route::resource('employees', EmployeeController::class)->except(['show', 'create', 'store', 'destroy']);
 
-});
-
-
-Route::ddd('clients')->group(function () {
-
-    Route::get('clients/dataTables', 'ClientController@dataTables')->name('clients.dataTables');
-    Route::patch('clients/{client}/password', 'ClientController@updatePassword')->name('clients.update.password');
-    Route::resource('clients', 'ClientController')->except(['show']);
-
-});
+Route::get('clients/dataTables', [ClientController::class, 'dataTables'])->name('clients.dataTables');
+Route::patch('clients/{client}/password', [ClientController::class, 'updatePassword'])->name('clients.update.password');
+Route::resource('clients', ClientController::class)->except(['show']);
