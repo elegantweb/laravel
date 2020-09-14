cae0b8092d7eebdd5ac047f2a802c1d180269220
Sina <sharifzadesina@gmail.com>
2020-09-14 15:54:21 +0430

    Update create.blade.php

--- resources/views/admin/clients/forms/create.blade.php
+++ resources/views/admin/clients/forms/create.blade.php
@@ -5,7 +5,7 @@
     <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
         <label for="name">Name</label>
         <input id="name" type="text" class="form-control"
-               name="name" value="{{ old('name') }}" required>
+                name="name" value="{{ old('name') }}" required>
         @error('name')
             <span class="help-block">
                 {{ $message }}
@@ -16,7 +16,7 @@
     <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
         <label for="email">Email</label>
         <input id="email" type="email" class="form-control"
-               name="email" value="{{ old('email') }}" required>
+                name="email" value="{{ old('email') }}" required>
         @error('email')
             <span class="help-block">
                 {{ $message }}
