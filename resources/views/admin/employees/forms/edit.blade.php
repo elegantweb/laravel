96ce118a4749b150aa5042c7e5efae79104530ef
Sina <sharifzadesina@gmail.com>
2020-09-14 15:53:33 +0430

    Update edit.blade.php

--- resources/views/admin/employees/forms/edit.blade.php
+++ resources/views/admin/employees/forms/edit.blade.php
@@ -6,13 +6,13 @@
     <div class="form-group">
         <label for="id">ID</label>
         <input id="id" type="text" class="form-control"
-            value="{{ $employee->id }}" disabled>
+                value="{{ $employee->id }}" disabled>
     </div>
 
     <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
         <label for="name">Name</label>
         <input id="name" type="text" class="form-control"
-               name="name" value="{{ old('name', $employee->name) }}" required>
+                name="name" value="{{ old('name', $employee->name) }}" required>
         @error('name')
             <span class="help-block">
                 {{ $errors->first('name') }}
@@ -23,7 +23,7 @@
     <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
         <label for="email">Email</label>
         <input id="email" type="email" class="form-control"
-               name="email" value="{{ old('email', $employee->email) }}" required>
+                name="email" value="{{ old('email', $employee->email) }}" required>
         @error('email')
             <span class="help-block">
                 {{ $errors->first('email') }}
